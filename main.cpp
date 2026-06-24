ee9991e035991e1e25a49e78dcf98b4712bbdd59
charankonada2006-ops <charankonada2006@gmail.com>
2026-06-24 09:01:05 +0530

    Add CGPA calculator in main.cpp
    Implement CGPA calculator that takes subjects, credits, and grade points as input.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,36 @@
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int subjects;
+    float credits, gradePoint;
+    float totalCredits = 0, totalGradePoints = 0, cgpa;
+
+    cout << "===== CGPA Calculator =====" << endl;
+
+    cout << "Enter number of subjects: ";
+    cin >> subjects;
+
+    for (int i = 1; i <= subjects; i++)
+    {
+        cout << "\nSubject " << i << endl;
+
+        cout << "Enter Credits: ";
+        cin >> credits;
+
+        cout << "Enter Grade Point (0-10): ";
+        cin >> gradePoint;
+
+        totalCredits += credits;
+        totalGradePoints += (credits * gradePoint);
+    }
+
+    cgpa = totalGradePoints / totalCredits;
+
+    cout << "\nTotal Credits = " << totalCredits << endl;
+    cout << "CGPA = " << cgpa << endl;
+
+    return 0;
+}
